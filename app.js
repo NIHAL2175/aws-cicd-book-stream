919ffded3272102a0fa0f15d4743c4340c0bffbb
NIHAL2175 <nihaal21075@gmail.com>
2026-05-16 22:16:24 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-db.connect((err) => {
-   if (err) {
-      console.error('Error connecting to MySQL: ' + err.stack);
-      return;
-   }
-   console.log('Connected to MySQL Database');
-});
+//db.connect((err) => {
+//   if (err) {
+//      console.error('Error connecting to MySQL: ' + err.stack);
+//      return;
+//   }
+//   console.log('Connected to MySQL Database');
+//});
 
 // Add your routes here
 app.use('/', routes);
--- configs/db.js
+++ configs/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
    host: 'localhost',
    port: '3306',
    user: 'root',
-   password: 'Insshaallah@24680',
+   password: 'DB_PASSWORD',
    database: 'react_node_app'
 });
 
